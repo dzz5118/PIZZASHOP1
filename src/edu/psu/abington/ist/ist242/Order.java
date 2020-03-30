0f9f6a41396f4ebb4b988fbfc7c9e225e3d4af3d
dzz5118 <dzz5118@psu.edu>
2020-03-29 22:58:08 -0400

    order

--- src/edu/psu/abington/ist/ist242/Order.java
+++ src/edu/psu/abington/ist/ist242/Order.java
@@ -7,9 +7,12 @@ public class Order {
     private int orderId;
     private Customer cust;
     private ArrayList<Menu> menuItem;
+    private static ArrayList<Order> orderList = new ArrayList<>();
+
 
     //Constructor Method
     public Order(int _orderId){
+
         this.orderId = _orderId;
     }
 
@@ -17,6 +20,33 @@ public class Order {
     public int getorderId() { return orderId; }
     public void setorderId(int _orderId) {this.orderId = _orderId;}
 
-    
+    public static void addOrders(Order order, Customer customer, ArrayList<Menu> mList){
+        order.setorderId(order.getorderId());
+        order.setCust(customer);
+        order.setMenuItem(mList);
+        orderList.add(order);
+        System.out.println("What you ordered : ");
+        for(Menu menu : mList){
+            System.out.println(menu.getmenuItem());
+        }
+        System.out.println("Your Name : " + order.getCust().getCustomerName() + ", Your ID : " + order.getCust().getCustomerId());
+    }
 
+    public ArrayList<Menu> getMenuItem() {
+        return menuItem;
+    }
+
+    public void setMenuItem(ArrayList<Menu> menuItem) {
+        this.menuItem = menuItem;
+    }
+
+    public Customer getCust() {
+        return cust;
+    }
+
+    public void setCust(Customer cust) {
+        this.cust = cust;
+    }
 }
+
+
